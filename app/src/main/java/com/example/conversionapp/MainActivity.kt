af7ce04ca694211efdb349f613f4d783853672ab
Ida Delphine <idadelm@gmail.com>
2024-05-20 18:37:43 +0100

    add logic to convert inches to centimeters

--- app/src/main/java/com/example/conversionapp/MainActivity.kt
+++ app/src/main/java/com/example/conversionapp/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private val inchValue = 2.54
     private lateinit var enterInches: EditText
     private lateinit var convertButton: Button
     private lateinit var textViewCentimeters: TextView
@@ -26,5 +27,14 @@ class MainActivity : AppCompatActivity() {
         enterInches = findViewById(R.id.editTextInches)
         convertButton = findViewById(R.id.button_convert)
         textViewCentimeters = findViewById(R.id.textViewConvert)
+
+        convertButton.setOnClickListener {
+            if (!enterInches.text.toString().isEmpty()) {
+                val result = enterInches.text.toString().toDouble() * inchValue
+                textViewCentimeters.text = result.toString()
+            } else {
+                textViewCentimeters.text = getString(R.string.text)
+            }
+        }
     }
 }
